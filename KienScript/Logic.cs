c046f56cee4bff66128253271fe3946e672163a8
Smarti MD <martijneau@live.nl>
2017-11-30 21:06:56 +0100

    refactored and fixed

---
+++ KienScript/Logic.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace KienScript
+{
+    class Logic
+    {
+        private Application OWord;
+        private Document document;
+
+        private object missing = System.Reflection.Missing.Value;
+
+        public Logic()
+            => CreateDocument();
+
+        private void CreateDocument()
+        {
+            //create instance of word
+            OWord = new Application
+            {
+                ShowAnimation = false,
+                Visible = false
+            };
+
+            //create new document
+            document = OWord.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+        }
+
+        public void GenerateDocument(int amountOfTables)
+        {
+            for (int i = 0; i < amountOfTables; i++)
+            {
+                int[,] dataSet = CreateTable();
+
+                AddTableToDocument(dataSet);
+            }
+
+            OWord.Visible = true;
+            OWord.ShowAnimation = true;
+        }
+
+        private int[,] CreateTable()
+        {
+            Random random = new Random();
+
+            int[,] dataSet = new int[3, 9];
+
+            List<int> value = new List<int> { 0, 0, 0 };
+
+            for (int index1 = 0; index1 < 9; index1++)
+            {
+                do
+                {
+                    value[0] = random.Next(1, 10);
+                    value[1] = random.Next(1, 10);
+                    value[2] = random.Next(1, 10);
+                } while (value.Count != value.Distinct().Count());
+
+                value.Sort();
+
+                for (int index0 = 0; index0 < 3; index0++)
+                {
+                    dataSet[index0, index1] = index1 * 10 + value[index0];
+                }
+            }
+
+            //for (int row = 0; row > 3; row++)
+            //{
+            //    dataSet[row, random.Next(0, 9)] = 0;
+            //}
+
+
+            int currentAmount;
+            int targetAmount;
+
+
+            for (int row = 0; row < 3; row++)
+            {
+                targetAmount = 23 - row * 4;
+                do
+                {
+                    dataSet[row, random.Next(0, 9)] = 0;
+
+                    currentAmount = dataSet.Cast<int>().Count(VARIABLE => VARIABLE > 0);
+                } while (currentAmount > targetAmount);
+            }
+            return dataSet;
+        }
+
+        private void AddTableToDocument(int[,] dataSet)
+        {
+            //initiate table
+            Range tableLocation = document.Range(document.Content.End - 1, ref missing);
+            document.Tables.Add(tableLocation, 3, 9, ref missing, ref missing);
+
+            //create table
+            Table table = document.Tables[document.Tables.Count];
+            foreach (Row row in table.Rows)
+            {
+                foreach (Cell cell in row.Cells)
+                {
+                    cell.Range.Text = dataSet[cell.RowIndex - 1, cell.ColumnIndex - 1] != 0 ?
+                        dataSet[cell.RowIndex - 1, cell.ColumnIndex - 1].ToString() : "";
+
+                    cell.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
+                }
+            }
+
+            document.Paragraphs.Add(ref missing).Range.InsertParagraphAfter();
+        }
+    }
+}
--- KienScript/Program.cs
+++ KienScript/Program.cs
@@ -13,95 +13,105 @@ namespace KienScript
     {
         static void Main(string[] args)
         {
-            string request;
+            string amountOfTables;
 
             do
             {
                 Console.Write("Geef het aantal kienkaarten: ");
-                request = Console.ReadLine();
-            } while (!int.TryParse(request, out int result));
+                amountOfTables = Console.ReadLine();
+            } while (!int.TryParse(amountOfTables, out int result));
 
-            Console.WriteLine("Loading...");
-            CreateFile(Convert.ToInt32(request));
+
+            //try
+            //{
+            Logic logic = new Logic();
+
+            logic.GenerateDocument(Convert.ToInt32(amountOfTables));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
         }
 
-        private static void CreateFile(int requestedAmount)
-        {
-            //TODO: standaard 15 cijfers laten printen
-            try
-            {
-                //create instance of word app
-                Application winword = new Application
-                {
 
-                    //set animation status for word application
-                    ShowAnimation = false,
-                    //set status for word application is to be visible or not
-                    Visible = false
-                };
+        //private static void CreateFile(int requestedAmount)
+        //{
+        //    //TODO: standaard 15 cijfers laten printen
+        //    try
+        //    {
+        //        //create instance of word app
+        //        Application winword = new Application
+        //        {
 
-                //create a missing variable for missing value
-                object missing = System.Reflection.Missing.Value;
+        //            //set animation status for word application
+        //            ShowAnimation = false,
+        //            //set status for word application is to be visible or not
+        //            Visible = false
+        //        };
 
-                //create new word document
-                Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+        //        //create a missing variable for missing value
+        //        object missing = System.Reflection.Missing.Value;
 
-                //Ensure randomness per card
-                Random random = new Random();
-                List<string> usedValues = new List<string>();
+        //        //create new word document
+        //        Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+        //        //Ensure randomness per card
+        //        Random random = new Random();
+        //        List<string> usedValues = new List<string>();
 
 
-                for (int currentAmount = 0; currentAmount < requestedAmount; currentAmount++)
-                {
+        //        for (int currentAmount = 0; currentAmount < requestedAmount; currentAmount++)
+        //        {
 
-                    //initiate table
-                    Range tableLocation = document.Range(document.Content.End - 1, ref missing);
-                    document.Tables.Add(tableLocation, 3, 9, ref missing, ref missing);
+        //            //initiate table
+        //            Range tableLocation = document.Range(document.Content.End - 1, ref missing);
+        //            document.Tables.Add(tableLocation, 3, 9, ref missing, ref missing);
 
-                    //create table
-                    Table table = document.Tables[document.Tables.Count];
-                    foreach (Row row in table.Rows)
-                    {
-                        foreach (Cell cell in row.Cells)
-                        {
-                            //create random value
-                            int secondInt = random.Next(0, 13);
-                            if (secondInt == 0 | secondInt >= 11)
-                                cell.Range.Text = "";
+        //            //create table
+        //            Table table = document.Tables[document.Tables.Count];
+        //            foreach (Row row in table.Rows)
+        //            {
+        //                foreach (Cell cell in row.Cells)
+        //                {
+        //                    //create random value
+        //                    int secondInt = random.Next(0, 13);
+        //                    if (secondInt == 0 | secondInt >= 11)
+        //                        cell.Range.Text = "";
 
-                            else if (secondInt == 10)
-                                cell.Range.Text = (cell.ColumnIndex).ToString() + "0";
+        //                    else if (secondInt == 10)
+        //                        cell.Range.Text = (cell.ColumnIndex).ToString() + "0";
 
-                            else if (cell.ColumnIndex == 1)
-                            {
-                                cell.Range.Text = secondInt.ToString();
-                            }
-                            else
-                            {
-                                cell.Range.Text = ((cell.ColumnIndex - 1).ToString()) + (secondInt.ToString());
-                            }
+        //                    else if (cell.ColumnIndex == 1)
+        //                    {
+        //                        cell.Range.Text = secondInt.ToString();
+        //                    }
+        //                    else
+        //                    {
+        //                        cell.Range.Text = ((cell.ColumnIndex - 1).ToString()) + (secondInt.ToString());
+        //                    }
 
-                            if (usedValues.Contains(cell.Range.Text))
-                                cell.Range.Text = "";
+        //                    if (usedValues.Contains(cell.Range.Text))
+        //                        cell.Range.Text = "";
 
-                            usedValues.Add(cell.Range.Text);
-                            cell.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
-                        }
-                    }
+        //                    usedValues.Add(cell.Range.Text);
+        //                    cell.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleSingle;
+        //                }
+        //            }
 
-                    usedValues.Clear();
-                    document.Paragraphs.Add(ref missing).Range.InsertParagraphAfter();
-                }
+        //            usedValues.Clear();
+        //            document.Paragraphs.Add(ref missing).Range.InsertParagraphAfter();
+        //        }
 
-                winword.Visible = true;
-            }
+        //        winword.Visible = true;
+        //    }
 
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.ReadKey();
-            }
-        }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        Console.ReadKey();
+        //    }
+        //}
 
     }
 }
